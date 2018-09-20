1a4ffeb7da4365d40b290a9ebe9bb7fb67aa77f5
hans <hrn@eal.dk>
2018-09-20 16:56:05 +0200

    helper class tilføjet

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,6 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git1");
+            Console.WriteLine("ny ændring");
             Console.ReadLine();
         }
     }
